d941d13a9e7f956621ed0443ab91896c240fcad0
prajna prayas <32980302+Prajna1999@users.noreply.github.com>
2022-03-18 16:33:32 +0000

    Initial commit

--- index.js
+++ index.js
@@ -8,29 +8,3 @@ const stocks={
 };
 const is_shop_open=false;
 
-const order=(work, time)=>{
-  return new Promise((resolve, reject)=>{
-    if(!is_shop_open){
-      // resolve and a set time out
-      setTimeout(()=>{
-         resolve(work())
-      }, time)
-     
-    }else{
-      // reject
-      reject(console.log("Our Shop is closed!"));
-    }
-  }
-)
-}
-
-order(()=>console.log(`${stocks.fruits[0]} was selected.`),2000)
-.then(()=>{
-  return order (()=>console.log("Food preparation has started."),0000)
-})
-.then(()=>order(()=>console.log("Fruit was chopped"),2000))
-.then(()=>order(()=>console.log(`${stocks.liquids[0]} and ${stocks.liquids[1]} were added.`),1000))
-.then(()=>order(()=>console.log("Start the machine"),2000))
-.then(()=>order(()=>console.log(`${stocks.holder[0]} was selected!`),2000))
-.then(()=>order(()=>console.log("Here's your ice cream."),3000));
-
