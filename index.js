99c865b7fde1fc2f2c8ee33fb985a197b2a9dcb8
prajna prayas <32980302+Prajna1999@users.noreply.github.com>
2022-03-18 16:26:47 +0000

    Add: promise chaining.

--- index.js
+++ index.js
@@ -6,54 +6,31 @@ const stocks={
   holder:["Cone", "Cup", "Stick"],
   toppings:["Chocolate", "Peanuts"]
 };
+const is_shop_open=false;
 
-
-// place order.
-const placeOrder=(fruitNameIndex,productionCallback)=>{
-
-  setTimeout(()=>{
-    
-    console.log(`${stocks.fruits[fruitNameIndex] } was selected.`);
-     productionCallback();
-  },3000)
-
- 
-}
-// Execution Order:outward>inward
-// production call back.
-const production=()=>{
-  setTimeout(()=>{
-    // parent setTimeOut 
-    console.log("Production has started!");
-
-    // leveltwo
-    setTimeout(()=>{
-      console.log("The fruit has been chopped!");
-
-      // level three. Add water/ice.
+const order=(work, time)=>{
+  return new Promise((resolve, reject)=>{
+    if(!is_shop_open){
+      // resolve and a set time out
       setTimeout(()=>{
-        console.log(`${stocks.liquids[0]}and ${stocks.liquids[1]} was added.`);
-
-        // level four.
-        setTimeout(()=>{
-          console.log("The machine has been started");
-
-          // Level five.
-          setTimeout(()=>{
-            console.log(`${stocks.holder[0]} was seleceted.`);
-
-            // Level six.
-            setTimeout(()=>{
-              console.log(`${stocks.toppings[1]} were added as toppings.`);
-              // Level seven. Serve the ice cream.
-              setTimeout(()=>{
-                console.log("Conratulations. Here's your order.")
-              },5000)
-            },3000)
-          },2000)
-        },1000)
-      },1000)
-    },2000)
-  },0000)
+         resolve(work())
+      }, time)
+     
+    }else{
+      // reject
+      reject(console.log("Our Shop is closed!"));
+    }
+  }
+)
 }
-placeOrder(0,production);
+
+order(()=>console.log(`${stocks.fruits[0]} was selected.`),2000)
+.then(()=>{
+  return order (()=>console.log("Food preparation has started."),0000)
+})
+.then(()=>order(()=>console.log("Fruit was chopped"),2000))
+.then(()=>order(()=>console.log(`${stocks.liquids[0]} and ${stocks.liquids[1]} were added.`),1000))
+.then(()=>order(()=>console.log("Start the machine"),2000))
+.then(()=>order(()=>console.log(`${stocks.holder[0]} was selected!`),2000))
+.then(()=>order(()=>console.log("Here's your ice cream."),3000));
+
